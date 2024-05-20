f273fe6859a80d940892584ce2c20fd3c140b068
Renan Reis <renanmacfernandes@hotmail.com>
2024-05-20 00:52:52 -0300

    ajustes de controladores

--- src/main/java/com/cadastro/cadastro/controllers/AgendaController.java
+++ src/main/java/com/cadastro/cadastro/controllers/AgendaController.java
@@ -1,8 +1,10 @@
 package com.cadastro.cadastro.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +13,16 @@ import com.cadastro.cadastro.services.AgendaService;
 
 
 @RestController
-@RequestMapping(name = "v1/agenda")
+@RequestMapping( "v1/agenda")
 public class AgendaController {
 	
-//	@Autowired
-//	private AgendaService agendaService;
-//	
-//	@PostMapping
-//	public ResponseEntity<Agenda> criar(Agenda agenda){
-//		Agenda entidade = agendaService.criar(agenda);
-//		return ResponseEntity.ok().body(entidade);
-//	}
+	@Autowired
+	private AgendaService agendaService;
+	
+	@PostMapping
+	public ResponseEntity<Agenda> criar(@RequestBody Agenda agenda){
+		Agenda entidade = agendaService.criar(agenda);
+		return ResponseEntity.status(HttpStatus.CREATED).body(entidade);
+	}
 
 }
