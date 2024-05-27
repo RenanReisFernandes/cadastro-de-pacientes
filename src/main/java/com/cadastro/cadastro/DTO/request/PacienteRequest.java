bfab00e7cc9c3002feb357fc84c7d8cca93d0f73
Renan Reis <renanmacfernandes@hotmail.com>
2024-05-26 23:51:14 -0300

    PacienteRequest validation

--- src/main/java/com/cadastro/cadastro/DTO/request/PacienteRequest.java
+++ src/main/java/com/cadastro/cadastro/DTO/request/PacienteRequest.java
@@ -1,5 +1,6 @@
 package com.cadastro.cadastro.DTO.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,13 @@ import lombok.Setter;
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class PacienteRequest {
 	
+	@NotBlank(message = "Nome não pode ser vazio")
 	private String nome;
+	@NotBlank(message = "Sobrenome não pode ser vazio")
 	private String sobrenome;
+	@NotBlank(message = "CPF não pode ser vazio")
 	private String cpf;
+	@NotBlank(message = "Email não pode ser vazio")
 	private String email;
 
 }
